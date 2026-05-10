523624c611209add097cb9e7216e1d3e9626f786
bash <hermensb@gmail.com>
2026-05-10 18:23:54 +0200

    fix(Core/Travel): Exclude NAV_GROUND_STEEP at all bot PathGenerator sites

--- src/Ai/Base/Actions/GoAction.cpp
+++ src/Ai/Base/Actions/GoAction.cpp
@@ -124,6 +124,7 @@ bool GoAction::Execute(Event event)
         if (botAI->HasStrategy("debug move", BOT_STATE_NON_COMBAT))
         {
             PathGenerator path(bot);
+            path.AddExcludeFlag(NAV_GROUND_STEEP);
 
             path.CalculatePath(x, y, z, false);
 
